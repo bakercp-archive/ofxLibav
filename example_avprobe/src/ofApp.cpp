5d3e698c2004b8f0fc2c5f1bad7ba5cfb1c6839a
Christopher Baker <me@christopherbaker.net>
2013-08-23 11:23:32 -0500

    Example updates.

--- example_avprobe/src/ofApp.cpp
+++ example_avprobe/src/ofApp.cpp
@@ -1,64 +1,51 @@
-#include "testApp.h"
+// =============================================================================
+//
+// Copyright (c) 2009-2013 Christopher Baker <http://christopherbaker.net>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// =============================================================================
 
-//--------------------------------------------------------------
-void testApp::setup(){
+
+#include "ofApp.h"
+
+
+//------------------------------------------------------------------------------
+void ofApp::setup()
+{
     ofxAVProbe::probe(ofFile("0_fingers.mov"));
 }
 
-//--------------------------------------------------------------
-void testApp::update(){
-    
+//------------------------------------------------------------------------------
+void ofApp::update()
+{
 }
 
 //--------------------------------------------------------------
-void testApp::draw(){
+void ofApp::draw()
+{
     ofBackground(0);
-    
     ofDrawBitmapString("Drag a Media File Onto the Window:", 15,15);
-    
 }
 
-//--------------------------------------------------------------
-void testApp::keyPressed(int key){
-    
-}
-
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){
-    
-}
-
-//--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-    
-}
-
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-    
-}
-
-//--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-    
-}
-
-//--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-    
-}
-
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-    
-}
-
-//--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
-    
-}
-
-//--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){
-    
+//------------------------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo)
+{
+        ofxAVProbe::probe(ofFile(dragInfo.files[0]));
 }
